8e88c81a42922a31ad68c3fd8818437c74ad644c
CHINAMI-A2H1KH9\Administrator <2243665175@qq.com>
2024-10-09 17:30:37 +0800

    第三天进度50%

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
         'line-1-rev': 'animate-line-1-rev 0.7s cubic-bezier(0.4, 0, 0.2, 1) forwards',
         'line-2-rev': 'animate-line-2-rev 0.7s cubic-bezier(0.4, 0, 0.2, 1) forwards',
         'line-3-rev': 'animate-line-3-rev 0.7s cubic-bezier(0.4, 0, 0.2, 1) forwards',
+        'sun-goes-down': 'sun-goes-down 4s infinite',
+        'sun-goes':  'sun-goes 4s infinite '
       },
       keyframes:{
         'line-1': {
@@ -20,7 +22,6 @@ module.exports = {
           '50%': { transform: 'translate3d(0, 10px, 0) rotate(0deg)' },
           '100%': { transform: 'translate3d(0, 10px, 0) rotate(45deg)' },
         },
-
         'line-2': {
           '0%': { transform: 'scale(1)', opacity:1 },
           '100%': { transform: 'scale(0)' ,opacity:0 },
@@ -30,13 +31,11 @@ module.exports = {
           '50%': { transform: 'translate3d(0, -10px, 0) rotate(0deg)' },
           '100%': { transform: 'translate3d(0, -10px, 0) rotate(135deg)' },
         },
-
         'animate-line-1-rev': {
           '0%': { transform: 'translate3d(0, 10px, 0) rotate(45deg)' },
           '50%': { transform: 'translate3d(0, 10px, 0) rotate(0deg)' },
           '100%': { transform: 'translate3d(0, 0, 0) rotate(0deg)' },
         },
-
         'animate-line-2-rev': {
           '0%': { transform: 'scale(0)', opacity:0 },
           '100%': { transform: 'scale(1)' ,opacity:1},
@@ -46,6 +45,22 @@ module.exports = {
           '50%': { transform: 'translate3d(0, -10px, 0) rotate(0deg)' },
           '100%': { transform: 'translate3d(0, 0, 0) rotate(0deg)' },
         },
+        'sun-goes-down':{
+          '0%':{opacity:100},
+          '50%':{opacity:100},
+          '100%':{opacity:0},
+        },
+        'sun-goes':{
+          '0%':{
+          transform: 'translate(-100%, 250%) rotate(0deg)'
+          },
+          '50%':{
+            transform: 'translate(200%, 0%) rotate(0deg)'
+          },
+          '100%':{
+            transform: 'translate(450%, 230%) rotate(0deg)'
+          },
+        }
       }
     },
   },
